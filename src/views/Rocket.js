3017d1aca010b9bd9e1f4eaa41dfc5eea331d09e
Pannita <60070061@kmitl.ac.th>
2021-02-15 19:14:46 +0700

    create modal

--- src/views/Rocket.js
+++ src/views/Rocket.js
@@ -1,11 +1,34 @@
-import react from 'react'
+import Card from "../components/Card";
+import Modal from "../components/Modals";
+import { Button, ButtonToolbar } from "rsuite";
+import { useState } from "react";
+
+const divStyle = {
+  backgroundColor: "black",
+  height: "100vh",
+  paddingTop: "60px",
+};
 
 const Rocket = () => {
-    return(
-        <div>
-            <h1>Rocket Page</h1>
-        </div>
-    )
-}
+  const [stateShowModal, setShowModal] = useState(false);
 
-export default Rocket
+  const setShouldUpdateModal = () => {
+    setShowModal(!stateShowModal)
+  }
+
+  return (
+    <div style={divStyle}>
+      <h1>Rocket Page</h1>
+      <div>
+        {/* {elements} */}
+        <Card />
+      </div>
+      <ButtonToolbar>
+        <Button onClick={setShouldUpdateModal}> Open</Button>
+      </ButtonToolbar>
+      <Modal showModal={stateShowModal} updateModalValue={setShowModal}/>
+    </div>
+  );
+};
+
+export default Rocket;

b3f180a85728d2e0c08b7af3089bfccb53668ca6
prapawit patthasirivichot <60070045@kmitl.ac.th>
2021-02-17 11:02:01 +0700

    add call api in launch page

--- src/views/Launch.js
+++ src/views/Launch.js
@@ -1,15 +1,36 @@
-import react from 'react'
+import axios from "axios";
+import react, { useEffect, useState } from "react";
 const divStyle = {
-    backgroundColor: 'black',
-    height: '100vh',
-    paddingTop: '60px'
-}
+  backgroundColor: "black",
+  height: "100vh",
+  paddingTop: "60px",
+};
 const Launch = () => {
-    return(
-        <div style={divStyle}>
-            <h1>Launch Page</h1>
-        </div>
-    )
-}
+  const [data, setData] = useState([]);
+  const [showContent, setShowContent] = useState(false);
 
-export default Launch
+  useEffect(() => {
+    const fetchData = async () => {
+      await axios("https://api.spacexdata.com/v3/launches").then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+    };
+
+    fetchData();
+  }, []);
+  return (
+    <div style={divStyle}>
+      <ul>
+        {data.map((launch) => (
+          <div>
+            <h1>{launch.flight_number}</h1>
+            <h2>{launch.mission_name}</h2>
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Launch;
